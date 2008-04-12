4664767b846196160860e7723dac6d705a7c1642
kangax <juriyzaytsev@iBook.local>
2008-04-11 20:45:07 -0400

    Added Form#unserialize. Need this to manage form state (undo/redo)

--- helpers.js
+++ helpers.js
@@ -259,4 +259,32 @@ Object._isFunction2 = function(o) {
     .call(o).indexOf('Function') != -1;
 }
 
+/**
+ * Form#unserialize(@element, source) -> Element
+ * - @element(FormElement): Form element to fill with values
+ * - source(Object | String): Source object where field values are taken from
+ *
+ * Fills form with values of a given object `source`. 
+ * Each propertie name of `source` is compared to name attribute of a form element.
+ *
+ *    $('myForm').unserialize()
+ *
+ **/
+Form.Methods.unserialize = function(element, source) {
+  if (!(element = $(element)))
+    throw new Error('DOMElement is required');
+  
+  source = Object.isString(source) 
+    ? source.toQueryParams() 
+    : source;
+  
+  element.getElements().each(function(element) {
+    for (var name in source) {
+      if (name == element.name)
+        element.setValue(source[name]);
+    }
+  })
+  return element;
+}
+
 Element.addMethods();
