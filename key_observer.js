9a6e68f6964f7c90372b430f527bb365dbbd61b6
kangax <juriyzaytsev@iBook.local>
2008-04-13 14:10:35 -0400

    document key observer

--- key_observer.js
+++ key_observer.js
@@ -1,17 +1,17 @@
-/*
-
-Example:
-
-document.observe('key:esc', helpWindow.close);
-document.observe('key:space', focusNextField);
-
-*/
- 
-
+/**
+ * Fires document-wide key:* events
+ * Observed keys are the ones from Event.KEY_*,
+ * as well as "space" and "pause" ones
+ *
+ *    document.observe('key:esc', helpWindow.close);
+ *    document.observe('key:space', focusNextField);
+ *
+ **/
 (function() {
   var hash = { 32: 'space', 19: 'pause' };
-  var handler = function(e) {
+  function handler(e) {
     if (!hash[e.keyCode]) return;
+    // tab needs keydown
     if (e.type == 'keydown' && e.keyCode != Event.KEY_TAB) return;
     document.fire('key:' + hash[e.keyCode], e);
   }
