33686b7e04fe1395e061c5cf2a1b1e92b697a04b
kangax <juriyzaytsev@iBook.local>
2008-04-27 15:09:08 -0400

    Add Class.Observable mixin

---
+++ class.observable.js
@@ -0,0 +1,31 @@
+/**
+ * mixin Class.Observable
+ *
+ *    var Person = Class.create(Class.Observable, {
+ *      initialize: function(name){ this.name = name }
+ *    })
+ *
+ *    var jdd = new Person('John');
+ *
+ *    jdd.observe(':say', console.log);
+ *    jdd.fire(':say');
+ *
+ **/
+ 
+// with corrections by John David Dalton
+(function(){
+  function getElement(object){
+    return (object._eventElement = object._eventElement || new Element('code'));
+  }
+  Class.Observable = {
+    observe: function(eventName, handler) {
+      getElement(this).observe(eventName, handler);
+    },
+    stopObserving: function() {
+      getElement(this).stopObserving.apply(null, arguments);
+    }
+    fire: function(eventName) {
+      getElement(this).fire(eventName);
+    }
+  }
+})();
