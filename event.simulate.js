5f1c70ca094d8a12aff8ff05de5d098495f3d313
kangax <juriyzaytsev@iBook.local>
2008-05-01 00:50:05 -0400

    Event.simulate refactoring

--- event.simulate.js
+++ event.simulate.js
@@ -1,3 +1,13 @@
+/**
+ * Event.simulate(@element, eventName[, options]) -> Element
+ * 
+ * - @element: element to fire event on
+ * - eventName: name of event to fire (only MouseEvents and HTMLEvents interfaces are supported)
+ * - options: optional object to fine-tune event properties - pointerX, pointerY, ctrlKey, etc.
+ *
+ *    $('foo').simulate('click'); // => fires "click" event on an element with id=foo
+ *
+ **/
 (function(){
   
   var eventMatchers = {
@@ -19,7 +29,9 @@
   Event.simulate = function(element, eventName) {
     var options = Object.extend(defaultOptions, arguments[2] || { });
     var oEvent, eventType = null;
-
+    
+    element = $(element);
+    
     for (var name in eventMatchers) {
       if (eventMatchers[name].test(eventName)) eventType = name;
     }
@@ -35,15 +47,18 @@
       else {
         oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView, 
           options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
-          options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, $(element));
+          options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
       }
-      $(element).dispatchEvent(oEvent);
+      element.dispatchEvent(oEvent);
     }
     else {
       options.clientX = options.pointerX;
       options.clientY = options.pointerY;
       oEvent = Object.extend(document.createEventObject(), options);
-      $(element).fireEvent('on' + eventName, oEvent);
+      element.fireEvent('on' + eventName, oEvent);
     }
+    return element;
   }
+  
+  Element.addMethods({ simulate: Event.simulate });
 })()
