2ed4d7038a5278e192efb2e2ca9e4a04f560d7d4
kangax <juriyzaytsev@ny3-dhcp-94.ithaka.org>
2008-05-14 18:16:38 -0400

    Add Function.K

--- function.extensions.js
+++ function.extensions.js
@@ -6,7 +6,7 @@
  * $$('*').findAll(function(element) { return !element.visible() }); // old way
  * $$('*').findAll(Element.visible.negate()); // using negate
  *
- */
+ **/
 Function.prototype.negate = function() {
   var f = this;
   return function() {
@@ -23,7 +23,7 @@ Function.prototype.negate = function() {
  * 
  * // new way
  * input.observe('change', myElement.toggle.runOnce());
- */
+ **/
 Function.prototype.runOnce = function() {
   this.apply(this, arguments);
   return this;
@@ -40,10 +40,30 @@ Function.prototype.runOnce = function() {
  * 
  * Person._new('Foo Bar');
  *
- */
+ **/
 Function.prototype._new = function() {
   var __method = this, args = arguments;
   function C() { return __method.apply(this, args); };
   C.prototype = __method.prototype;
   return new C;
 };
+
+/**
+ * Creates a function which returns specified value.
+ * Useful as a closure "wrapper" for blocks of code 
+ * which need to have certain variables stored in their own closure (rather than sharing them)
+ *
+ * for (var prop in source) {
+ *   dest[prop] = function(){ return String(prop) } // <= "prop" is "shared" among all methods
+ * }
+ *
+ * for (var prop in source) {
+ *   dest[prop] = Function.K(String(prop)); // <= now each "prop" is stored in Function.K's closure
+ * }
+ *
+ **/
+Function.K = function(k){
+  return function() {
+    return k;
+  }
+}
