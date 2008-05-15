e040f3ead00ce4b6d21d1f88fd9529941ffee74b
kangax <juriyzaytsev@iBook.local>
2008-05-14 21:17:19 -0400

    Add somewhat type-safe Array#sum

--- array.extensions.js
+++ array.extensions.js
@@ -14,3 +14,13 @@ Array.prototype.namespace = function(parent) {
     return object[property] = object[property] || { };
   })
 };
+
+/**
+ * [1,2,3].sum(); 6
+ * ['5','','8', 10].sum(); 23 <= type-safe
+ *
+ **/
+Array.prototype.sum = function() {
+  for (var sum=i=0, l=this.length; i<l; i++) { sum-= this[i]; }
+  return -sum;
+};
