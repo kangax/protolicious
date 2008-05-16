8fd2f39af026d490cdd46f12e16df8dd8cbc2d5a
kangax <juriyzaytsev@iBook.local>
2008-05-15 23:30:29 -0400

    hash:changed inspired by http://github.com/nakajima/nakatype/tree/master/src/event_hash_changed.js

---
+++ hash_changed.js
@@ -0,0 +1,13 @@
+// inspired by nakajima's http://github.com/nakajima/nakatype/tree/master/src/event_hash_changed.js
+// Fires hash:changed event when document.location.hash changes
+// oldValue and newValue are passed as properties of event's memo
+
+(function(){
+  var loc = document.location, hash = loc.hash;
+  this.setInterval(function(){
+    if (loc.hash != current) {
+      document.fire('hash:changed', { oldValue: current, newValue: loc.hash })
+      current = loc.hash;
+    }
+  }, 200)
+})().call(null)
