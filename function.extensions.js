93fd66d02d1e9b49c55db600a344ba99cd3bca02
kangax <juriyzaytsev@iBook.local>
2008-05-19 15:57:58 -0400

    Add Function#toDelayed/toDeferred

--- function.extensions.js
+++ function.extensions.js
@@ -49,6 +49,35 @@ Function.prototype._new = function() {
 };
 
 /**
+ * Function#toDelayed(timeout) -> Function
+ * - timeout(Number): interval to call function within (in seconds)
+ *
+ * Augments a function into a delayed one
+ *
+ * formElement.observe('reset', handler.toDelayed(1));
+ *
+ **/
+Function.prototype.toDelayed = function(timeout) {
+  var __method = this;
+  return function() {
+    var args = $A(arguments);
+    setTimeout(function(){ __method.apply(__method, args) }, timeout * 1000);
+  }
+};
+
+/**
+ * Function#toDeferred() -> Function
+ *
+ * Augments a function into a deferred one
+ *
+ * formElement.observe('reset', handler.toDeffered());
+ *
+ **/
+Function.prototype.toDeferred = function() {
+  return this.toDelayed(0.01);
+}
+
+/**
  * Creates a function which returns specified value.
  * Useful as a closure "wrapper" for blocks of code 
  * which need to have certain variables stored in their own closure (rather than sharing them)
