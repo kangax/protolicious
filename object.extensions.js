2528e7433b0a35df2853fa17b51a2872ec122f34
kangax <juriyzaytsev@iBook.local>
2008-05-19 11:06:21 -0400

    Add Object.isEvent, minor readme changes

--- object.extensions.js
+++ object.extensions.js
@@ -36,3 +36,28 @@ Object.directProperties = function(o) {
   }
   return result;
 }
+
+/**
+ * Object.isEvent(object) -> Boolean
+ * - object(Any): Object to test
+ *
+ * Returns true if passed object "partially" conforms to a DOM2 Event interface
+ * The method uses somewhat naive duck-typing by verifying types of some of the properties as per:
+ *
+ * http://www.w3.org/TR/DOM-Level-2-Events/events.html#Events-Event
+ *
+ * Most of the time, though, we want to either stop event, invoke #element method or 
+ * access some specific property (.target, .currentTarget, etc.)
+ * In those cases, it would make sense to test for those particular methods (before calling them)
+ *
+ * Maybe Object.isExtendedEvent is the way to go?
+ *
+ *    function toggleAll(e) {
+ *      if (Object.isEvent(e)) e.stop();
+ *      // $$('div.toggleable').invoke('toggle');
+ *    }
+ *
+ **/
+Object.isEvent = function(object) {
+  return object && Object.isString(object.type) && Object.isBoolean(object.bubbles);
+}
